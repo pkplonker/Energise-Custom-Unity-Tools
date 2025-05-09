d2d3f9e6fd3d904f5ccda5652e414132238da680
stuart <pkplonker@gmail.com>
2025-05-09 22:32:54 +0100

    ui improvements

--- Assets/Energise Software/Scriptable Object Editor/Editor/ScriptableObjectEditor.cs
+++ Assets/Energise Software/Scriptable Object Editor/Editor/ScriptableObjectEditor.cs
@@ -5,246 +5,217 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using NUnit.Framework;
 
 namespace ScriptableObjectEditor
 {
-	public partial class ScriptableObjectEditorWindow : EditorWindow
-	{
-		private Vector2 scrollPosition;
-		private List<Type> scriptableObjectTypes;
-		private string[] typeNames;
-		private int selectedTypeIndex;
+    public partial class ScriptableObjectEditorWindow : EditorWindow
+    {
+        private Vector2 scrollPosition;
+        private List<Type> scriptableObjectTypes;
+        private string[] typeNames;
+        private int selectedTypeIndex;
 
-		private readonly List<ScriptableObject> currentTypeObjects = new();
-		private static string assetsFolderPath = "Assets";
+        private List<ScriptableObject> currentTypeObjects = new();
+        private static string assetsFolderPath = "Assets";
 
-		private List<Assembly> availableAssemblies;
-		private string[] assemblyNames;
-		private int selectedAssemblyIndex;
+        private List<Assembly> availableAssemblies;
+        private string[] assemblyNames;
+        private int selectedAssemblyIndex;
 
-		private bool includeDerivedTypes = true;
-		private DateTime lastAssemblyCheckTime = DateTime.Now;
-		private int draggingColumn = -1;
-		private float dragStartMouseX;
-		private float dragStartWidth;
-		private List<float> columnWidths = new List<float>();
+        private bool includeDerivedTypes = true;
+        private DateTime lastAssemblyCheckTime = DateTime.Now;
+        private int draggingColumn = -1;
+        private float dragStartMouseX;
+        private float dragStartWidth;
+        private List<float> columnWidths = new List<float>();
 
-		[MenuItem("Window/Energise Tools/Scriptable Object Editor &%S")]
-		public static void ShowWindow() => GetWindow<ScriptableObjectEditorWindow>("Scriptable Object Editor");
-		
-		static ScriptableObjectEditorWindow()
-		{
-			AssemblyReloadEvents.afterAssemblyReload += OnAfterAssemblyReload;
-		}
+        // New search strings
+        private string typeSearchString = string.Empty;
+        private string instanceSearchString = string.Empty;
 
-		private static void OnAfterAssemblyReload()
-		{
-			var window = GetWindow<ScriptableObjectEditorWindow>();
-			window.LoadAvailableAssemblies();
-			window.LoadScriptableObjectTypes();
-			window.LoadObjectsOfType(window.scriptableObjectTypes.FirstOrDefault());
-			window.Repaint();
-		}
+        [MenuItem("Window/Energise Tools/Scriptable Object Editor &%S")]
+        public static void ShowWindow() => GetWindow<ScriptableObjectEditorWindow>("Scriptable Object Editor");
 
-		private void OnEnable()
-		{
-			LoadAvailableAssemblies();
-			LoadScriptableObjectTypes();
-			LoadObjectsOfType(scriptableObjectTypes.FirstOrDefault());
-		}
+        static ScriptableObjectEditorWindow()
+        {
+            AssemblyReloadEvents.afterAssemblyReload += OnAfterAssemblyReload;
+        }
 
-		/// <summary>
-		/// Reloads the assemblies
-		/// </summary>
-		private void LoadAvailableAssemblies()
-		{
-			availableAssemblies = GetAssembliesWithScriptableObjects();
+        private static void OnAfterAssemblyReload()
+        {
+            var window = GetWindow<ScriptableObjectEditorWindow>();
+            window.LoadAvailableAssemblies();
+            window.LoadScriptableObjectTypes();
+            window.LoadObjectsOfType(window.scriptableObjectTypes.FirstOrDefault());
+            window.Repaint();
+        }
 
-			assemblyNames = availableAssemblies
-				.Select(assembly => assembly.GetName().Name)
-				.Prepend("All Assemblies")
-				.ToArray();
-		}
+        private void OnEnable()
+        {
+            LoadAvailableAssemblies();
+            LoadScriptableObjectTypes();
+            LoadObjectsOfType(scriptableObjectTypes.FirstOrDefault());
+        }
 
-		/// <summary>
-		/// Gets all assemblies containing a scriptable object
-		/// </summary>
-		/// <returns>The collection of assemblies</returns>
-		private static List<Assembly> GetAssembliesWithScriptableObjects()
-		{
-			return AppDomain.CurrentDomain.GetAssemblies()
-				.Where(assembly =>
-					!assembly.FullName.StartsWith("UnityEngine") &&
-					!assembly.FullName.StartsWith("UnityEditor") &&
-					!assembly.FullName.StartsWith("Unity."))
-				.Where(assembly =>
-					assembly.GetTypes().Any(type =>
-						type.IsSubclassOf(typeof(ScriptableObject)) && !type.IsAbstract && IsInAssetsFolder(type)))
-				.OrderBy(assembly => assembly.GetName().Name)
-				.ToList();
-		}
+        private void LoadAvailableAssemblies()
+        {
+            availableAssemblies = GetAssembliesWithScriptableObjects();
+            assemblyNames = availableAssemblies
+                .Select(assembly => assembly.GetName().Name)
+                .Prepend("All Assemblies")
+                .ToArray();
+        }
 
-		/// <summary>
-		/// Gets all the types derived from <see cref="scriptableObjectTypes"/>
-		/// </summary>
-		private void LoadScriptableObjectTypes()
-		{
-			IEnumerable<Type> types;
+        private static List<Assembly> GetAssembliesWithScriptableObjects()
+        {
+            return AppDomain.CurrentDomain.GetAssemblies()
+                .Where(assembly =>
+                    !assembly.FullName.StartsWith("UnityEngine") &&
+                    !assembly.FullName.StartsWith("UnityEditor") &&
+                    !assembly.FullName.StartsWith("Unity."))
+                .Where(assembly =>
+                    assembly.GetTypes().Any(type =>
+                        type.IsSubclassOf(typeof(ScriptableObject)) && !type.IsAbstract && IsInAssetsFolder(type)))
+                .OrderBy(assembly => assembly.GetName().Name)
+                .ToList();
+        }
 
-			if (selectedAssemblyIndex == 0)
-			{
-				types = availableAssemblies.SelectMany(assembly => assembly.GetTypes());
-			}
-			else
-			{
-				types = availableAssemblies[selectedAssemblyIndex - 1].GetTypes();
-			}
+        private void LoadScriptableObjectTypes()
+        {
+            IEnumerable<Type> types = selectedAssemblyIndex == 0
+                ? availableAssemblies.SelectMany(a => a.GetTypes())
+                : availableAssemblies[selectedAssemblyIndex - 1].GetTypes();
 
-			scriptableObjectTypes = types
-				.Where(type =>
-					type.IsSubclassOf(typeof(ScriptableObject)) && !type.IsAbstract && IsInAssetsFolder(type))
-				.OrderBy(type => type.Name)
-				.ToList();
+            scriptableObjectTypes = types
+                .Where(t => t.IsSubclassOf(typeof(ScriptableObject)) && !t.IsAbstract && IsInAssetsFolder(t))
+                .OrderBy(t => t.Name)
+                .ToList();
 
-			typeNames = scriptableObjectTypes.Select(type => type.Name).ToArray();
-		}
+            // apply type search filter
+            if (!string.IsNullOrEmpty(typeSearchString))
+            {
+                scriptableObjectTypes = scriptableObjectTypes
+                    .Where(t => t.Name.IndexOf(typeSearchString, StringComparison.OrdinalIgnoreCase) >= 0)
+                    .ToList();
+            }
 
-		private static bool IsInAssetsFolder(Type type)
-		{
-			var guids = AssetDatabase.FindAssets($"t:{type.Name}", new[] {assetsFolderPath});
-			return guids.Any();
-		}
+            typeNames = scriptableObjectTypes.Select(t => t.Name).ToArray();
+            selectedTypeIndex = Mathf.Clamp(selectedTypeIndex, 0, typeNames.Length - 1);
+        }
 
-		/// <summary>
-		/// Adds to "currentTypeObjects"/> all objects of the provided type
-		/// </summary>
-		/// <param name="type">The type to load</param>
-		private void LoadObjectsOfType(Type type)
-		{
-			if (type == null)
-			{
-				currentTypeObjects.Clear();
-				return;
-			}
+        private static bool IsInAssetsFolder(Type type)
+        {
+            var guids = AssetDatabase.FindAssets($"t:{type.Name}", new[] {assetsFolderPath});
+            return guids.Any();
+        }
 
-			currentTypeObjects.Clear();
+        private void LoadObjectsOfType(Type type)
+        {
+            currentTypeObjects.Clear();
+            if (type == null) return;
 
-			var guids = AssetDatabase.FindAssets("t:ScriptableObject", new[] {assetsFolderPath});
-			foreach (var guid in guids)
-			{
-				var assetPath = AssetDatabase.GUIDToAssetPath(guid);
-				var obj = AssetDatabase.LoadAssetAtPath<ScriptableObject>(assetPath);
+            var guids = AssetDatabase.FindAssets("t:ScriptableObject", new[] {assetsFolderPath});
+            foreach (var guid in guids)
+            {
+                var path = AssetDatabase.GUIDToAssetPath(guid);
+                var obj = AssetDatabase.LoadAssetAtPath<ScriptableObject>(path);
+                if (obj == null) continue;
 
-				if (obj == null) continue;
-				if (includeDerivedTypes)
-				{
-					if (type.IsAssignableFrom(obj.GetType()))
-					{
-						currentTypeObjects.Add(obj);
-					}
-				}
-				else
-				{
-					if (obj.GetType() == type)
-					{
-						currentTypeObjects.Add(obj);
-					}
-				}
-			}
-		}
+                if ((includeDerivedTypes && type.IsAssignableFrom(obj.GetType())) ||
+                    (!includeDerivedTypes && obj.GetType() == type))
+                {
+                    currentTypeObjects.Add(obj);
+                }
+            }
 
-		/// <summary>
-		/// Updates the UI
-		/// </summary>
-		private void OnGUI()
-		{
-			scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
-			{
-				EditorGUILayout.BeginVertical("box");
-				{
-					EditorGUILayout.LabelField("Asset Management", EditorStyles.boldLabel);
+            // apply instance search filter
+            if (!string.IsNullOrEmpty(instanceSearchString))
+            {
+                currentTypeObjects = currentTypeObjects
+                    .Where(o => o.name.IndexOf(instanceSearchString, StringComparison.OrdinalIgnoreCase) >= 0)
+                    .ToList();
+            }
+        }
 
-					EditorGUILayout.BeginHorizontal();
-					{
-						EditorGUILayout.LabelField("Path", GUILayout.Width(40));
-						assetsFolderPath = EditorGUILayout.TextField(assetsFolderPath, GUILayout.Width(200));
+        private void OnGUI()
+        {
+            scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
+            EditorGUILayout.BeginVertical("box");
 
-						if (GUILayout.Button("Browse", GUILayout.Width(80)))
-						{
-							string selectedPath = EditorUtility.OpenFolderPanel("Select Scriptable Object Folder",
-								assetsFolderPath, "");
-							if (!string.IsNullOrEmpty(selectedPath))
-							{
-								assetsFolderPath = selectedPath.Replace(Application.dataPath, "Assets");
-								LoadScriptableObjectTypes();
-								LoadObjectsOfType(scriptableObjectTypes.FirstOrDefault());
-							}
-						}
+            EditorGUILayout.LabelField("Asset Management", EditorStyles.boldLabel);
+            // folder & assembly selection
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("Path", GUILayout.Width(40));
+            assetsFolderPath = EditorGUILayout.TextField(assetsFolderPath, GUILayout.Width(200));
+            if (GUILayout.Button("Browse", GUILayout.Width(80)))
+            {
+                string sel = EditorUtility.OpenFolderPanel("Select Scriptable Object Folder", assetsFolderPath, "");
+                if (!string.IsNullOrEmpty(sel))
+                {
+                    assetsFolderPath = sel.Replace(Application.dataPath, "Assets");
+                    LoadScriptableObjectTypes();
+                    LoadObjectsOfType(scriptableObjectTypes.FirstOrDefault());
+                }
+            }
+            
+            if (GUILayout.Button(EditorGUIUtility.IconContent("d_Refresh"), GUILayout.Width(35)))
+            {
+	            LoadAvailableAssemblies();
+	            LoadScriptableObjectTypes();
+	            LoadObjectsOfType(scriptableObjectTypes.FirstOrDefault());
+            }
+            int newAsm = EditorGUILayout.Popup(selectedAssemblyIndex, assemblyNames, GUILayout.Width(200));
+            if (newAsm != selectedAssemblyIndex)
+            {
+	            selectedAssemblyIndex = newAsm;
+	            LoadScriptableObjectTypes();
+	            LoadObjectsOfType(scriptableObjectTypes.FirstOrDefault());
+            }
+            EditorGUILayout.EndHorizontal();
 
-						EditorGUILayout.LabelField("Assembly", GUILayout.Width(60));
-						var newAssemblyIndex =
-							EditorGUILayout.Popup(selectedAssemblyIndex, assemblyNames, GUILayout.Width(200));
-						if (newAssemblyIndex != selectedAssemblyIndex)
-						{
-							selectedAssemblyIndex = newAssemblyIndex;
-							LoadScriptableObjectTypes();
-							selectedTypeIndex = 0;
-							LoadObjectsOfType(scriptableObjectTypes.FirstOrDefault());
-						}
+            EditorGUILayout.Space();
+            // type search and selection
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("Type", GUILayout.Width(40));
+            int newTypeIdx = EditorGUILayout.Popup(selectedTypeIndex, typeNames, GUILayout.Width(200));
+            if (newTypeIdx != selectedTypeIndex)
+            {
+	            selectedTypeIndex = newTypeIdx;
+	            LoadObjectsOfType(scriptableObjectTypes[selectedTypeIndex]);
+            }
+            includeDerivedTypes = EditorGUILayout.ToggleLeft("Include Derived", includeDerivedTypes, GUILayout.Width(120));
+            EditorGUILayout.LabelField("Search Types", GUILayout.Width(80));
 
-						if (GUILayout.Button("Refresh Assemblies", GUILayout.Width(150)))
-						{
-							LoadAvailableAssemblies();
-							LoadScriptableObjectTypes();
-							LoadObjectsOfType(scriptableObjectTypes.FirstOrDefault());
-						}
-					}
-					EditorGUILayout.EndHorizontal();
+            var newTypeSearch = EditorGUILayout.TextField(typeSearchString, GUILayout.Width(200));
+            if (newTypeSearch != typeSearchString)
+            {
+                typeSearchString = newTypeSearch;
+                LoadScriptableObjectTypes();
+                LoadObjectsOfType(scriptableObjectTypes.FirstOrDefault());
+            }
+            
+            EditorGUILayout.LabelField("Search Instances", GUILayout.Width(100));
+            var newInstSearch = EditorGUILayout.TextField(instanceSearchString, GUILayout.Width(200));
+            if (newInstSearch != instanceSearchString)
+            {
+                instanceSearchString = newInstSearch;
+                LoadObjectsOfType(scriptableObjectTypes[selectedTypeIndex]);
+            }
+            // if (GUILayout.Button("Load Objects", GUILayout.Width(100)))
+            // {
+            //     LoadObjectsOfType(scriptableObjectTypes[selectedTypeIndex]);
+            // }
+            EditorGUILayout.EndHorizontal();
 
-					EditorGUILayout.Space();
+            EditorGUILayout.Space();
+            if (currentTypeObjects.Any() && typeNames.Any())
+            {
+                DrawPropertiesGrid();
+            }
 
-					EditorGUILayout.BeginHorizontal();
-					{
-						EditorGUILayout.LabelField("Type", GUILayout.Width(40));
-						var newSelectedTypeIndex =
-							EditorGUILayout.Popup(selectedTypeIndex, typeNames, GUILayout.Width(200));
-						if (newSelectedTypeIndex != selectedTypeIndex)
-						{
-							selectedTypeIndex = newSelectedTypeIndex;
-							LoadObjectsOfType(scriptableObjectTypes[selectedTypeIndex]);
-						}
-
-						var newIncludeDerivedTypes =
-							EditorGUILayout.ToggleLeft("Include Derived", includeDerivedTypes, GUILayout.Width(120));
-						if (newIncludeDerivedTypes != includeDerivedTypes)
-						{
-							includeDerivedTypes = newIncludeDerivedTypes;
-							LoadObjectsOfType(scriptableObjectTypes[selectedTypeIndex]);
-						}
-
-						if (GUILayout.Button("Load Objects", GUILayout.Width(100)))
-						{
-							LoadObjectsOfType(scriptableObjectTypes[selectedTypeIndex]);
-						}
-					}
-					EditorGUILayout.EndHorizontal();
-
-					EditorGUILayout.Space();
-
-					if (currentTypeObjects.Any() && typeNames.Any())
-					{
-						EditorGUILayout.BeginVertical();
-						{
-							DrawPropertiesGrid();
-						}
-						EditorGUILayout.EndVertical();
-					}
-				}
-				EditorGUILayout.EndVertical();
-			}
-			EditorGUILayout.EndScrollView();
-		}
+            EditorGUILayout.EndVertical();
+            EditorGUILayout.EndScrollView();
+        }
 
 		private void DrawHeaderCell(string label, float width, int columnIndex)
 		{
